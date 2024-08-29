c21746fb429d5efa5b365065ed9406feacd34258
Alex G <ebooklub@gmail.com>
2024-08-29 15:54:56 +0000

    Initial commit of dbt project

---
+++ dbt_superstore/models/bronze/stg_returned_orders.sql
@@ -0,0 +1,11 @@
+{{ config(
+  materialized='view',
+  alias='returned_orders'
+) }}
+
+SELECT
+    order_id,
+    returned,
+    row_number() OVER () AS dwh_id,
+    current_timestamp AS etl_timestamp
+FROM {{ source('superstore', 'returns') }}
